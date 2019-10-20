712033c0f47b57a2e30704a68e59149b2f8dd9ed
Sander van Hooft <info@sandervanhooft.nl>
2019-10-20 14:04:11 +0200

    Prepare release v1.2.5

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -14,7 +14,7 @@ use Mollie\Laravel\MollieServiceProvider;
 
 class CashierServiceProvider extends ServiceProvider
 {
-    const PACKAGE_VERSION = '1.2.0';
+    const PACKAGE_VERSION = '1.2.5';
 
     /**
      * Bootstrap the application services.
