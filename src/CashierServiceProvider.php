b7a153a85ae463d0207a915664164321114ede6e
Sander van Hooft <info@sandervanhooft.nl>
2019-12-23 09:59:31 +0100

    Update version number

--- src/CashierServiceProvider.php
+++ src/CashierServiceProvider.php
@@ -14,7 +14,7 @@ use Mollie\Laravel\MollieServiceProvider;
 
 class CashierServiceProvider extends ServiceProvider
 {
-    const PACKAGE_VERSION = '1.10.0';
+    const PACKAGE_VERSION = '1.10.1';
 
     /**
      * Bootstrap the application services.
