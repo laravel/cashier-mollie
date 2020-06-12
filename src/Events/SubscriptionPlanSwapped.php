f31514974b5f015f40ec9088190a267cf28e05c4
Tobias Schulz <tobias@byte.software>
2020-06-12 10:22:15 +0200

    added previousPlan to SubscriptionPlanSwapped Event

--- src/Events/SubscriptionPlanSwapped.php
+++ src/Events/SubscriptionPlanSwapped.php
@@ -14,8 +14,17 @@ class SubscriptionPlanSwapped
      */
     public $subscription;
 
-    public function __construct(Subscription $subscription)
+    /**
+     * The previous subscription plan before swapping if exists.
+     *
+     * @var mixed
+     */
+    public $previousPlan;
+
+    public function __construct(Subscription $subscription, $previousPlan = null)
     {
         $this->subscription = $subscription;
+
+        $this->previousPlan = $previousPlan;
     }
 }
