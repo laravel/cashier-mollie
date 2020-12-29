9815198121d8d07021db567503e0cd29812df7d3
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2020-12-29 15:58:29 +0200

    change url for opengraph image

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,7 +67,7 @@ module.exports = {
         twitterCard: _ => 'summary_large_image',
         type: $page => 'website',
         url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
-        image: ($page, $site) => "./.vuepress/public/assets/pages/laravelcashiermollie.jpg",
+        image: ($page, $site) => "https://ciungulete.github.io/cashier-mollie/assets/img/laravelcashiermollie.jpg",
         publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
         modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
     }],
