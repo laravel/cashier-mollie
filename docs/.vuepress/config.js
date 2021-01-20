da4809cf61cfb54d4ed3108426655cdfd66ad968
Andrei Ciungulete <andrei.ciungulete@andali.ro>
2021-01-20 23:29:59 +0200

    wip

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,11 @@ module.exports = {
   title: 'Laravel Cashier Mollie v2.x',
   description: 'Laravel Cashier provides an expressive, fluent interface to subscriptions using Mollie\'s billing services.',
   head: [
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
-    ['link', { rel: "manifest", href: "/assets/favicons/manifest.json"}],
-    ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#ffffff"}],
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/manifest.json"}],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#ffffff"}],
     ['meta', { name: "theme-color", content: "#ffffff"}],
     ['meta', { name: "viewport", content: "width=device-width"}],
     ['script', { src: "https://cdn.usefathom.com/script.js", spa: "auto", site: "ANMLOYPH", defer:true}]
