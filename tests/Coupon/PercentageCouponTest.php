5d239e361eaeb4d02edd29a2fc14e83347dd70a0
Gert Timmerman <gtimmerman@fritsjurgens.com>
2021-01-21 17:54:57 +0100

    Add Percentage Discount Handler

---
+++ tests/Coupon/PercentageCouponTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Laravel\Cashier\Tests\Coupon;
+
+use Laravel\Cashier\Coupon\AppliedCoupon;
+use Laravel\Cashier\Coupon\Contracts\CouponRepository;
+use Laravel\Cashier\Coupon\Coupon;
+use Laravel\Cashier\Coupon\CouponOrderItemPreprocessor;
+use Laravel\Cashier\Coupon\PercentageDiscountHandler;
+use Laravel\Cashier\Order\OrderItem;
+use Laravel\Cashier\Order\OrderItemCollection;
+use Laravel\Cashier\Subscription;
+use Laravel\Cashier\Tests\BaseTestCase;
+
+class PercentageCouponTest extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withPackageMigrations();
+    }
+
+    /** @test */
+    public function couponCalculatesTheRightPrice()
+    {
+        $couponHandler = new PercentageDiscountHandler;
+
+        $context = [
+            'description' => 'Percentage coupon',
+            'percentage' => 20,
+        ];
+
+        $coupon = new Coupon(
+            'percentage-coupon',
+            $couponHandler,
+            $context
+        );
+
+        $this->withMockedCouponRepository($coupon, $couponHandler, $context);
+
+        /** @var Subscription $subscription */
+        $subscription = factory(Subscription::class)->create();
+        $item = factory(OrderItem::class)->make();
+        $subscription->orderItems()->save($item);
+
+        /** @var \Laravel\Cashier\Coupon\Coupon $coupon */
+        $coupon = app()->make(CouponRepository::class)->findOrFail('percentage-coupon');
+        $redeemedCoupon = $coupon->redeemFor($subscription);
+        $preprocessor = new CouponOrderItemPreprocessor();
+
+        $result = $preprocessor->handle($item->toCollection());
+
+        $this->assertEquals(-2952, $result[1]->unit_price);
+    }
+}
